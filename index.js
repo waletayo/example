6bb5ee62e863679fa1b88bbb6b30be47d289ba63
Adeeyo adewale temitayo <tayot51@gmail.com>
2021-11-06 11:23:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
+
+
+app.use("/", (req, res, next) => {
+    return res.json("welcome")
+})
+
+
 app.listen(port, () => {
     console.log(`App running on port ${port}`, '...');
 });
